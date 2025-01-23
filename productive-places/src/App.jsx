fa9ed537808f1c5502cca5284c6b00a2779d4797
Yomna Elmousalami <yelmo001@odu.edu>
2025-01-22 23:27:02 -0500

    added buttons for future features like navigaton

--- productive-places/src/App.jsx
+++ productive-places/src/App.jsx
@@ -17,6 +17,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div class = "nav_buttons">
+        <button class = "Home">Home</button>
+        <button class = "search">Search by Filter</button>
+        <button class = "places">Places</button>
+        <button class = "contact">Contact</button>
+        <button class = "signUp">Sign-Up</button>
+        <button class = "signIn">Sign-in</button>
+      </div>
       <h2 class = "subtitle">Let's Find your favorite study spot</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)} class = "countBttn">
