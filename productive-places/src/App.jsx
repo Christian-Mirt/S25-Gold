6977b41bce3f5766c56608e2d729b1a1968269ef
Yomna Elmousalami <yelmo001@odu.edu>
2025-01-24 09:52:38 -0500

    adjusted buttons and removed react logo

--- productive-places/src/App.jsx
+++ productive-places/src/App.jsx
@@ -13,9 +13,6 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <div class = "nav_buttons">
         <button class = "Home">Home</button>
