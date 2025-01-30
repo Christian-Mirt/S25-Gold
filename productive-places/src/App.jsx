d971f89c23286570832e8eede8dc128537b0bc9f
Yomna Elmousalami <yelmo001@odu.edu>
2025-01-30 08:54:37 -0500

    change "class" to "className", moved signup html and css code to seperate files

--- productive-places/src/App.jsx
+++ productive-places/src/App.jsx
@@ -1,35 +1,16 @@
 import { useState } from 'react'
+import { StrictMode } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './SignUp.jsx'
+import SignUp from './SignUp.jsx'
 
 function App() {
 
-  return (
-    <>
-      <h1 class = "Main_Title">Productive Places</h1>
-      <div class = "nav_buttons">
-        <button class = "Home">Home</button>
-        <button class = "search">Search by Filter</button>
-        <button class = "places">Places</button>
-        <button class = "contact">Contact</button>
-        <button class = "signUp">Sign-Up</button>
-        <button class = "signIn">Sign-in</button>
-      </div>
-      <input type="text" placeholder="Search.." class = "searchBttn"></input>
-      <div class = "loginInfo">
-        <form action = "">
-          <h1 class = "signHeader">Sign Up</h1>
-          <input type = "text" placeholder='Username' required class = "user"></input><br/>
-          <input type = "text" placeholder='Password' required class = "pass"></input><br/><br />
-          <label class = "remember"><input id="rememberme" name="rememberme" value="remember" type="checkbox"/>Remember Me</label><br/> 
-          <a href = "#" class = "forgot">Forgot Password?</a>
-        </form>
-      </div>
-      <h2 class = "subtitle">Let's Find your favorite study spot</h2>
-      <div className="card">
-        <button type = "submit" class = "signBttn">Sign Up</button>
-      </div>
-    </>
+  return(
+    <div>
+      <SignUp />
+    </div>
   )
 }
 
---
+++ productive-places/src/SignUp.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './SignUp.css'
+
+function SignUp(){
+    return (
+        <>
+          <h1 className = "Main_Title">Productive Places</h1>
+          <div className = "nav_buttons">
+            <button className = "Home">Home</button>
+            <button className = "search">Search by Filter</button>
+            <button className = "places">Places</button>
+            <button className = "contact">Contact</button>
+            <button className = "signUp">Sign-Up</button>
+            <button className = "signIn">Sign-in</button>
+          </div>
+          <input type="text" placeholder="Search.." class = "searchBttn"></input>
+          <div className = "loginInfo">
+            <form action = "">
+              <h1 className = "signHeader">Sign Up</h1>
+              <input type = "text" placeholder='Username' required class = "user"></input><br/>
+              <input type = "text" placeholder='Password' required class = "pass"></input><br/><br />
+              <label className = "remember"><input id="rememberme" name="rememberme" value="remember" type="checkbox"/>Remember Me</label><br/> 
+              <a href = "#" class = "forgot">Forgot Password?</a>
+            </form>
+          </div>
+          <h2 className = "subtitle">Let's Find your favorite study spot</h2>
+          <div className="card">
+            <button type = "submit" className = "signBttn">Sign Up</button>
+          </div>
+        </>
+      )
+}
+
+export default SignUp
