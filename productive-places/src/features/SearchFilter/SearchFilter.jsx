b63f69f459752d7a106bafa1e30cafe96f29fb76
Yomna Elmousalami <yelmo001@odu.edu>
2025-01-31 12:32:37 -0500

    styled most of search filter

--- productive-places/src/features/SearchFilter/SearchFilter.jsx
+++ productive-places/src/features/SearchFilter/SearchFilter.jsx
@@ -6,7 +6,14 @@ function SearchFilter() {
 
   return( 
     <div>
-      <h1>Hello World!</h1>
+      <input type="text" placeholder="Search for a place..." className = "bigSearch"></input>
+      <h1 className='browseHeader'>Or browse using the filters below</h1>
+      <input type="text" placeholder="Rating" className = "rating"></input>
+      <input type="text" placeholder="Hours" className = "hours"></input>
+      <input type="text" placeholder="Wifi" className = "wifi"></input>
+      <input type="text" placeholder="Location" className = "loc"></input>
+      <input type="text" placeholder="Popular Times" className = "pop"></input>
+      <input type="text" placeholder="Food & Drink" className = "food"></input>
     </div>
   )
 }
