fea1b3196dfd5076003d5d398e50e0589a21199f
Yomna Elmousalami <yelmo001@odu.edu>
2025-01-31 12:45:12 -0500

    Finish Login styling (note: almost exact same styling as signup page)

--- productive-places/src/features/SignIn/SignIn.jsx
+++ productive-places/src/features/SignIn/SignIn.jsx
@@ -5,9 +5,21 @@ function SignIn() {
   const navigate = useNavigate();
 
   return( 
-    <div>
-      <h1>Hello World!</h1>
+    <>
+    <div className = "loginInfo">
+      <form action = "">
+        <h1 className = "loginHeader">Login</h1>
+        <input type = "text" placeholder='Username' required className = "user"></input><br/>
+        <input type = "password" placeholder='Password' required className = "pass"></input><br/><br />
+        <label className = "remember"><input id="rememberme" name="rememberme" value="remember" type="checkbox"/>Remember Me</label><br/> 
+        <a href = "#" className = "forgot">Forgot Password?</a>
+      </form>
     </div>
+    <h2 className = "subtitle">Let's find your next favorite study spot!</h2>
+    <div className="card">
+      <button type = "submit" className = "signBttn">Login</button>
+    </div>
+  </>
   )
 }
 
