89dd593a7449e2191f50a26e5ba6df668a517dfe
mevan028 <mevan028@odu.edu>
2025-02-02 10:22:08 -0500

    testing api

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,22 +2,9 @@ import { Router } from "express";
 import { connection } from "../database/database.js";
 const user = Router();
 
-user.get("/", (req, res) => {
-    connection.execute("select * from user_information", function (err, result) {
-        if (err) {
-            res.json(err.message);
-        } else {
-            res.json({
-                status: 200,
-                message: "Response from user get api",
-                data: result,
-            });
-        }
-    });
-});
-
-user.get("/:id", (req, res) => {
-    connection.execute("select * from user_information where user_id=?",
+user.post("/:id", (req, res) => {
+    connection.execute(
+        "select * from user_information where user_id=?",
         [req.params.id],
         function (err, result) {
             if (err) {
