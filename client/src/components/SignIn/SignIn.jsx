7f77c413260104d295c2b58d539a684270743bb5
mevan028 <mevan028@odu.edu>
2025-02-03 07:07:23 -0500

    debug

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -41,7 +41,8 @@ export default function SignIn() {
 
     if (data.status === 200) {
       console.log(data);
-      // setTotalUsers(data.total_users);
+      console.log(data[0]);
+      setTotalUsers(data[0]);
     } else {
       console.log("Error fetching total users");
     }
