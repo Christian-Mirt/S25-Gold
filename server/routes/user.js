11a5066609edb5729c6e00a280e4d2a7f48a9f5c
mevan028 <mevan028@odu.edu>
2025-02-04 07:24:31 -0500

    add get version of total

--- server/routes/user.js
+++ server/routes/user.js
@@ -37,4 +37,22 @@ user.post("/total", (req, res) => {
     );
 });
 
+user.get("/get-total", (req, res) => {
+    connection.execute(
+        "select count(*) as total_users from user_information",
+        [],
+        function (err, result) {
+            if (err) {
+                res.json(err.message);
+            } else {
+                res.json({
+                    status: 200,
+                    message: "Total number of users retrieved successfully",
+                    data: result[0].total_users,
+                });
+            }
+        }
+    );
+});
+
 export default user;
