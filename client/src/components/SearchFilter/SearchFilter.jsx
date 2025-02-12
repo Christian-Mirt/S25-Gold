adf91d2c68506d41c09c322d1c2fa4fcac76fe62
YomnaElmousalami <yomnaelmousalami@gmail.com>
2025-02-12 16:27:11 -0500

    change search filters to drop downs

--- client/src/components/SearchFilter/SearchFilter.jsx
+++ client/src/components/SearchFilter/SearchFilter.jsx
@@ -8,12 +8,30 @@ function SearchFilter() {
     <div>
       <input type="text" placeholder="Search for a place..." className = "bigSearch"></input>
       <h1 className='browseHeader'>Or browse using the filters below</h1>
-      <input type="text" placeholder="Rating" className = "rating"></input>
-      <input type="text" placeholder="Hours" className = "hours"></input>
-      <input type="text" placeholder="Wifi" className = "wifi"></input>
-      <input type="text" placeholder="Location" className = "loc"></input>
-      <input type="text" placeholder="Popular Times" className = "pop"></input>
-      <input type="text" placeholder="Food & Drink" className = "food"></input>
+      <select type="text" placeholder="Rating" className = "rating">
+        <option value="" selected disabled>Rating</option>
+        <option value="1">Option 1</option>
+      </select>
+      <select type="text" placeholder="Hours" className = "hours">
+        <option value="" selected disabled>Hours</option>
+        <option value="1">Option 1</option>
+      </select>
+      <select type="text" placeholder="Wifi" className = "wifi">
+        <option value="" selected disabled>Wifi</option>
+        <option value="1">Option 1</option>
+      </select>
+      <select type="text" placeholder="Location" className = "loc">
+        <option value="" selected disabled>Location</option>
+        <option value="1">Option 1</option>
+      </select>
+      <select type="text" placeholder="Popular Times" className = "pop">
+        <option value="" selected disabled>Popular Times</option>
+        <option value="1">Option 1</option>
+      </select>
+      <select type="text" placeholder="Food & Drink" className = "food">
+        <option value="" selected disabled>Food & Drink</option>
+        <option value="1">Option 1</option>
+      </select>
       <button className='searchButton'>Search</button>
     </div>
   )
