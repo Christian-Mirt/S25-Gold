d7b5ce6edecbc5479a596275821dfe62ffc4a1c5
YomnaElmousalami <yomnaelmousalami@gmail.com>
2025-02-12 22:37:19 -0500

    trying to connect to places table

--- client/src/components/Places/Places.jsx
+++ client/src/components/Places/Places.jsx
@@ -1,12 +1,36 @@
 import './Places.css'
+import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 function Places() {
   const navigate = useNavigate();
+   const [totalUsers, setTotalUsers] = useState(0);
+
+     const getTotalUsers = async () => {
+       const result = await fetch(import.meta.env.VITE_API_KEY + '/user/tps', {
+         method: "POST",
+         headers: {
+           'content-type': 'application/json',
+         },
+       });
+   
+       const data = await result.json();
+   
+       if (data.status === 200) {
+         setTotalUsers(data.data);
+       } else {
+         console.log("Error fetching total users");
+       }
+     };
+   
+     useEffect(() => {
+       getTotalUsers();
+     }, []);
 
   return( 
     <div>
       <h1>Results for</h1>
+      <p><b>Number of users: </b> {totalUsers}</p>
     </div>
   )
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -90,4 +90,22 @@ user.post("/signIn", (req, res) => {
     );
 });
 
+user.post("/tps", (req, res) => {
+    connection.execute(
+        "select count(*) as tp from places",
+        [],
+        function (err, result) {
+            if (err) {
+                res.json(err.message);
+            } else {
+                res.json({
+                    status: 200,
+                    message: "Total number of users retrieved successfully",
+                    data: result[0].tp,
+                });
+            }
+        }
+    );
+});
+
 export default user;
