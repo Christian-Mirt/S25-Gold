b919f5c1aa260d2acf5849374b44ff5dd73631c1
Christian-Mirt <cmirt001@odu.edu>
2025-02-18 11:19:15 -0500

    Fixed styling on all except signIn/signUp

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -5,11 +5,13 @@ function HomePage() {
   const navigate = useNavigate();
 
   return( 
-    <div className = "main">
-      <h2 className = "caption">Let&apos;s find your next favorite study spot!</h2>
-      <button className="nextBttn">
-        <span onClick = {() => {navigate("/signup")}} className="material-symbols-outlined">arrow_forward</span>
-      </button>
+    <div className = "center-vertically">
+      <div className='center-horizontally card'>
+        <h2 className = "caption">Let&apos;s find your next favorite study spot!</h2>
+        <button className="nextBttn">
+          <span onClick = {() => {navigate("/signup")}} className="material-symbols-outlined">arrow_forward</span>
+        </button>
+      </div>
     </div>
   )
 }
--- client/src/components/SearchFilter/SearchFilter.jsx
+++ client/src/components/SearchFilter/SearchFilter.jsx
@@ -9,42 +9,44 @@ function SearchFilter() {
   }
 
   return( 
-    <div>
-      <input type="text" placeholder="Search for a place..." className = "bigSearch"></input>
-      <h1 className='browseHeader'>Or browse using the filters below</h1>
-      <select type="text" placeholder="Rating" className = "rating">
-        <option value="" selected disabled>Rating</option>
-        <option value="5">5</option>
-        <option value="4">4</option>
-        <option value="3">3</option>
-        <option value="2">2</option>
-        <option value="1">1</option>
-      </select>
-      <select type="text" placeholder="Hours" className = "hours">
-        <option value="" selected disabled>Hours</option>
-        <option value="1">Option 1</option>
-      </select>
-      <select type="text" placeholder="Wifi" className = "wifi">
-        <option value="" selected disabled>Wifi</option>
-        <option value="excellent">Excellent</option>
-        <option value="good">Good</option>
-        <option value="ok">Okay</option>
-        <option value="slow">Slow</option>
-        <option value="really_slow">Really Slow</option>
-      </select>
-      <select type="text" placeholder="Location" className = "loc">
-        <option value="" selected disabled>Location</option>
-        <option value="1">Option 1</option>
-      </select>
-      <select type="text" placeholder="Popular Times" className = "pop">
-        <option value="" selected disabled>Popular Times</option>
-        <option value="1">Option 1</option>
-      </select>
-      <select type="text" placeholder="Food & Drink" className = "food">
-        <option value="" selected disabled>Food & Drink</option>
-        <option value="1">Option 1</option>
-      </select>
-      <button className='searchButton' onClick={searchPlace}>Search</button>
+    <div className='center-vertically'>
+      <div className='center-horizontally'>
+        <input type="text" placeholder="Search for a place..." className = "bigSearch"></input>
+        <h1 className='browseHeader'>Or browse using the filters below</h1>
+        <select type="text" placeholder="Rating" className = "rating">
+          <option value="" selected disabled>Rating</option>
+          <option value="5">5</option>
+          <option value="4">4</option>
+          <option value="3">3</option>
+          <option value="2">2</option>
+          <option value="1">1</option>
+        </select>
+        <select type="text" placeholder="Hours" className = "hours">
+          <option value="" selected disabled>Hours</option>
+          <option value="1">Option 1</option>
+        </select>
+        <select type="text" placeholder="Wifi" className = "wifi">
+          <option value="" selected disabled>Wifi</option>
+          <option value="excellent">Excellent</option>
+          <option value="good">Good</option>
+          <option value="ok">Okay</option>
+          <option value="slow">Slow</option>
+          <option value="really_slow">Really Slow</option>
+        </select>
+        <select type="text" placeholder="Location" className = "loc">
+          <option value="" selected disabled>Location</option>
+          <option value="1">Option 1</option>
+        </select>
+        <select type="text" placeholder="Popular Times" className = "pop">
+          <option value="" selected disabled>Popular Times</option>
+          <option value="1">Option 1</option>
+        </select>
+        <select type="text" placeholder="Food & Drink" className = "food">
+          <option value="" selected disabled>Food & Drink</option>
+          <option value="1">Option 1</option>
+        </select>
+        <button className='searchButton' onClick={searchPlace}>Search</button>
+      </div>
     </div>
   )
 }
