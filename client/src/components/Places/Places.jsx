85f5b9fd1edb54ae5d36350dadcd8de4b0fdbfd8
Christian-Mirt <cmirt001@odu.edu>
2025-02-17 23:49:58 -0500

    Fixed body display issue and added to places tab

--- client/src/components/Places/Places.jsx
+++ client/src/components/Places/Places.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router-dom';
 
 function Places() {
   const navigate = useNavigate();
-  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPlaces, setTotalPlaces] = useState(0);
   const [allPlaces, setPlaces] = useState(null);
 
-     const getTotalUsers = async () => {
+     const getTotalPlaces = async () => {
        const result = await fetch(import.meta.env.VITE_API_KEY + '/place/tps', {
          method: "GET",
          headers: {
@@ -19,7 +19,7 @@ function Places() {
    
        if (data.status === 200) {
          console.log(data);
-         setTotalUsers(data.data);
+         setTotalPlaces(data.data);
        } else {
          console.log("Error fetching total users");
        }
@@ -48,27 +48,60 @@ function Places() {
    
      useEffect(() => {
        getPlaces();
-       getTotalUsers();
+       getTotalPlaces();
      }, []);
 
-  let ratingNum = 0;
   return( 
-    <div className="placesCatalog">
-      <h1>Places Catalog</h1>
-      <p><b>Number of places: </b> {totalUsers}</p>
-      <hr width="100%" size="2" color="white" noshade></hr>
-      {allPlaces ? (
-      <ul>
-        {allPlaces.map((place, index) => (
-          <div key={index}>
-            <p> {place.name}: &nbsp; {place.overall_rating}</p>
-            <hr width="100%" size="2" color="white" noshade></hr>
-          </div>
-        ))}
-      </ul>
-    ) : (
-      <p>No places available</p>
-    )}
+    <div className='page-container'>
+      <div className="places-catalog">
+        <h1>Places Catalog</h1>
+        <p><b>Number of places: </b> {totalPlaces}</p>
+        <hr width="100%" size="2" color="white" noshade></hr>
+        {allPlaces ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Rating</th>
+              <th>Place</th>
+              <th>Location</th>
+              <th>Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allPlaces.map((place, index) => (
+              <tr key={index}>
+                <td>
+                  <div className="star-rating">
+                    <div className="stars">
+                      {[...Array(5)].map((_, i) => (
+                        <span key={i} className="star">&#9733;</span> // Unicode star
+                      ))}
+                    </div>
+                    <div className="stars filled" style={{ width: `${(place.overall_rating / 5) * 100}%` }}>
+                      {[...Array(5)].map((_, i) => (
+                        <span key={i} className="star">&#9733;</span> // Unicode star
+                      ))}
+                    </div>
+                  </div>
+                </td>
+                <td>{place.name}</td>
+                <td>{place.city}: {place.address}</td>
+                <td>{place.hours ? place.hours : 'Hours not found'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No places available</p>
+      )}
+      </div>
+      <div className='place-details'>
+        <h1>Reviews</h1>
+        <div>
+          <h4>User Manny Pacquiao says:</h4>
+          <p>This place was not good.</p>
+        </div>
+      </div>
     </div>
   )
 }
