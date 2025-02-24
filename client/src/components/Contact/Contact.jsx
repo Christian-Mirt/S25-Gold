c2b88c9dd4fa5d5a0374b764c6de60ae7b92a4b3
YomnaElmousalami <yomnaelmousalami@gmail.com>
2025-02-24 00:22:14 -0500

    manual file upload functionality

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,16 +1,28 @@
 import './Contact.css'
 import { useNavigate } from 'react-router-dom';
+import { useRef, useEffect } from 'react';
 
 function Contact() {
   const navigate = useNavigate();
+  const profilePicRef = useRef(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.onchange = function () {
+      if (this.files.length > 0) {
+        profilePicRef.current.src = URL.createObjectURL(this.files[0]);
+      }
+    };
+  }, []);
 
   return( 
     <div className="contactCatalog">
-      <h1 className = "profileTitle"><div class="dot"></div>Your Profile</h1>
+      <h1 className = "profileTitle"><img src="about:blank" id = "pfp" ref={profilePicRef} className='dot'/>Your Profile</h1>
       <a href className='favs'>Your favorites</a><br/><br/>
       <a href className='reviews'>Your Reviews</a><br/><br/>
       <a href className='prefs'>Your Preferences</a><br/><br/>
-      <a href className='profilePhoto'>Upload a Profile Photo</a>
+      <label for = "input_file" className='profilePhoto'>Upload a Profile Photo</label>
+      <input type = "file" accept = "image/*" id = "input_file" ref={inputRef}></input>
     </div>
   )
 }
