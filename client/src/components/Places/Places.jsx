b2030c75f486df535842012374a4b61781eea589
Christian-Mirt <cmirt001@odu.edu>
2025-02-25 14:55:43 -0500

    Added amenities to places page

--- client/src/components/Places/Places.jsx
+++ client/src/components/Places/Places.jsx
@@ -9,30 +9,14 @@ function Places() {
   const ratingParam = queryParams.get('rating');
   const wifiParam = queryParams.get('wifi');
 
-  const [totalPlaces, setTotalPlaces] = useState(0);
   const [allPlaces, setPlaces] = useState(null);
   const [selectedPlace, setSelectedPlace] = useState(null);
-  const [selectedPlaceDetails, setSelectedPlaceDetails] = useState(null);
+  const [selectedPlaceAmenities, setSelectedPlaceAmenities] = useState(null);
+  const [selectedPlaceReviews, setSelectedPlaceReviews] = useState(null);
   const [isRefreshing, setIsRefreshing] = useState(false);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [sortOrder, setSortOrder] = useState('asc');
   const [alphabeticalOrder, setAlphabeticalOrder] = useState('asc');
-
-  const getTotalPlaces = async () => {
-    const result = await fetch(import.meta.env.VITE_API_KEY + '/place/tps', {
-      method: "GET",
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
-
-    const data = await result.json();
-    if (data.status === 200) {
-      setTotalPlaces(data.data);
-    } else {
-      console.log("Error fetching total places");
-    }
-  };
 
   const getPlaces = async () => {
     const url = import.meta.env.VITE_API_KEY + '/place/catalogs';
@@ -64,17 +48,34 @@ function Places() {
   };
 
   const getSelectedPlaceDetails = async (placeId) => {
-    const url = import.meta.env.VITE_API_KEY + '/place/reviews';
-    const response = await fetch(url + '?place_id=' + placeId, {
+    const url = import.meta.env.VITE_API_KEY + '/place/';
+    // Get the amenities
+    const amenitiesResponse = await fetch(url + 'amenities?place_id=' + placeId, {
       method: "GET",
       headers: {
         'content-type': 'application/json',
       },
     });
 
-    const data = await response.json();
-    if (data.status === 200) {
-      setSelectedPlaceDetails(data.data);
+    const amenities = await amenitiesResponse.json();
+    if (amenities.status === 200) {
+      setSelectedPlaceAmenities(amenities.data[0]);
+      console.log(amenities.data[0]);
+    } else {
+      console.log("Error fetching place details");
+    }
+
+    // Get the reviews
+    const reviewsResponse = await fetch(url + 'reviews?place_id=' + placeId, {
+      method: "GET",
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+
+    const reviews = await reviewsResponse.json();
+    if (reviews.status === 200) {
+      setSelectedPlaceReviews(reviews.data);
     } else {
       console.log("Error fetching place details");
     }
@@ -122,7 +123,6 @@ function Places() {
 
   useEffect(() => {
     getPlaces();
-    getTotalPlaces();
   }, [ratingParam, wifiParam]);
 
   return (
@@ -130,7 +130,7 @@ function Places() {
 
       <div className="places-catalog">
         <h1>Places Catalog</h1>
-        <p><b>Number of places: </b> {totalPlaces}</p>
+        <p><b>Number of places: </b> {filteredPlaces.length}</p>
         <hr width="100%" size="2" color="white" noshade></hr>
         {filteredPlaces.length > 0 ? (
           <table>
@@ -170,31 +170,51 @@ function Places() {
       </div>
       
       <div className={`place-details ${isRefreshing ? "refreshing" : ""}`}>
-        {selectedPlace && selectedPlaceDetails ? (
+        {selectedPlace ? (
           <div>
-            <h1>{selectedPlace.name} Reviews</h1>
-            {selectedPlaceDetails.map((review, index) => (
-              <div key={index}>
-                <h2>{review.first_name + ' ' + review.last_name}</h2>
-                <div className="star-rating">
-                  <div className="stars">
-                    {[...Array(5)].map((_, i) => (
-                      <span key={i} className="star">&#9733;</span>
-                    ))}
+            <h1>{selectedPlace.name} Details</h1>
+            <h2>Amenities</h2>
+            {selectedPlaceAmenities ? (
+              <ul>
+                {selectedPlaceAmenities.has_wifi ? <li><b>Free WiFi</b></li> : <></>}
+                {selectedPlaceAmenities.has_outlets ? <li><b>Available power outlets</b></li> : <></>}
+                {selectedPlaceAmenities.has_tables ? <li><b>Table space and seating available</b></li> : <></>}
+                {selectedPlaceAmenities.group_friendly ? <li><b>Group friendly</b></li> : <></>}
+                <li>Noise level: <b>{selectedPlaceAmenities.noise_level}</b></li>
+              </ul>
+            ) : (
+              <p>No amenities found</p>
+            )}
+            <hr />
+            {selectedPlaceReviews ? (
+              <div>
+                <h2>User Reviews</h2>
+                {selectedPlaceReviews.map((review, index) => (
+                  <div key={index}>
+                    <h3>{review.first_name + ' ' + review.last_name}</h3>
+                    <div className="star-rating">
+                      <div className="stars">
+                        {[...Array(5)].map((_, i) => (
+                          <span key={i} className="star">&#9733;</span>
+                        ))}
+                      </div>
+                      <div className="stars filled" style={{ width: `${(review.num_stars / 5) * 100}%` }}>
+                        {[...Array(5)].map((_, i) => (
+                          <span key={i} className="star">&#9733;</span>
+                        ))}
+                      </div>
+                    </div>
+                    <h4>{review.comment}</h4>
+                    <hr />
                   </div>
-                  <div className="stars filled" style={{ width: `${(review.num_stars / 5) * 100}%` }}>
-                    {[...Array(5)].map((_, i) => (
-                      <span key={i} className="star">&#9733;</span>
-                    ))}
-                  </div>
-                </div>
-                <h3>{review.comment}</h3>
-                <hr />
+                ))}
               </div>
-            ))}
+            ) : (
+              <h2>This place does not have any reviews yet.</h2>
+            )}
           </div>
         ) : (
-          <h2>This place does not have any reviews yet.</h2>
+          <h1>No place selected</h1>
         )}
       </div>
     </div>
