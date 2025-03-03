5175ad5200de7051900dc0031f45832e5055a3dc
YomnaElmousalami <yomnaelmousalami@gmail.com>
2025-03-03 09:43:08 -0500

    fix reset password

--- server/routes/reset.js
+++ server/routes/reset.js
@@ -2,21 +2,28 @@ import { Router } from "express";
 import { connection } from "../database/database.js";
 const reset = Router();
 
-reset.put("/generateNewPass", (req, res) =>{
+reset.put("/generateNewPass", (req, res) => {
+
     connection.execute(
         "update user_information set password=? where email=?",
         [req.body.password, req.body.email],
-        function (err, result){
-            if (err){
-                res.json();
-            }
-            else
-            {
-                res.json({
-                    status: 200,
-                    message: "Created New Password",
-                    data:result,
-                });
+        function (err, result) {
+            if (err) {
+                res.json(err.message);
+            } else {
+                if (result.affectedRows > 0) {
+                    res.json({
+                        status: 200,
+                        message: "Created new password",
+                        data: result,
+                    });
+                } else {
+                    res.json({
+                        status: 401,
+                        message: "No accounts found",
+                        data: result,
+                    });
+                }
             }
         }
     );
