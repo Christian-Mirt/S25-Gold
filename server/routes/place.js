80d81a9820dee12adca906f80f5d1c871e57f5e5
Jake Carosi Primes <jcaro005@odu.edu>
2025-03-24 18:00:01 -0400

    More review form

--- server/routes/place.js
+++ server/routes/place.js
@@ -84,8 +84,8 @@ place.get("/places", (req, res) => {
     const { name } = req.query;
     
     pool.execute(
-        "SELECT * FROM places WHERE name LIKE ?",
-        [`%${name}%`],
+        "SELECT * FROM places WHERE name = ?",
+        [{name}],
         (err, result) => {
             if (err) {
                 res.json({ error: err.message });
